92ad8d962abb4866b921ee6650fb6cc65c8e3c03
jitinder <sidak.pasrichassp@gmail.com>
2025-01-26 16:54:10 +0000

    feat: add plan page

---
+++ src/app/workout/plan/page.tsx
@@ -0,0 +1,10 @@
+export default function WorkoutPlanPage() {
+  return (
+    <div className="flex flex-col w-full h-screen items-center gap-4">
+      <iframe
+        src="https://v2-embednotion.com/18774ea48e28801abbf7fdc0efa46e36"
+        className="h-full w-full"
+      />
+    </div>
+  );
+}
